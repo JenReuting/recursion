aa72891d185cde189b3bb8558d200ac39ad15669
Jennifer Reuting <jen@reuting.com>
2022-12-13 16:20:12 -0800

    solved to binarySearchIndex

--- recursion.js
+++ recursion.js
@@ -53,45 +53,36 @@ function isPalindrome(str) {
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) { //"apple"
-  // if (str.length > 0) {
-  //   return str[str.length - 1] + revString(str.slice(0, -1));
-  // } else {
-  //   return ""; //this can be an empty string instead
-  // }
+function revString(str) {
 
-  if (str.length === 0) return ""; //base case
+  if (str.length === 0) return "";
 
   return revString(str.slice(1)) + str[0];
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) { //['duck', "cat", "cat", 'pony'] //val = donkey
+function findIndex(arr, val) {
   if (arr.length === 0) return -1;
 
   if (arr[0] === val) return 0;
 
-  // if (arr.length === 0) {
-  //   return -1;
-  // } else if (arr[0] === val) {
-  //   return 0;
-  // }
-
   return findIndex(arr.slice(1), val) === -1 ? -1 : 1 + findIndex(arr.slice(1), val);
-  // return findIndex(arr.slice(1), val) + 1;
-
-  ///// 1 + findIndex ("pony")
-  ////// 1 + findIndex ("cat", "pony")
-  /////// 1 + findIndex("cat", "cat", "pony")
-
-  //['duck', "cat", "cat", 'pony'] //val = pony
 
 }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
 function gatherStrings(obj) {
+  const strArr = [];
+  for (let key in obj) {
+    if (typeof obj[key] === "string") {
+      strArr.push(obj[key]);
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      strArr.push(...gatherStrings(obj[key]));
+    }
+  }
+  return strArr;
 
 }
 
@@ -100,9 +91,18 @@ function gatherStrings(obj) {
 /** binarySearch: given a sorted array of numbers, and a value,
  * return true if val is in array, false if not present). */
 
+
 function binarySearch(arr, val) {
 
+  if (arr.length === 0) return false;
+
+  let median = Math.floor((arr.length - 1) / 2);
+
+  if (arr[median] === val) return true;
+  if (arr[median] > val) return binarySearch(arr.slice(0, median), val);
+  if (arr[median] < val) return binarySearch(arr.slice(median + 1, arr.length), val);
 }
+
 
 
 /** binarySearch: given a sorted array of numbers, and a value,
